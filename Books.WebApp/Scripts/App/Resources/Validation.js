1262af7267560db457e3dd3b4f12f896a0cf5bfe
Alexey Marinyak <zeal314@gmail.com>
2019-12-21 17:12:44 +0300

    Fixed uploading image

--- Books.WebApp/Scripts/App/Resources/Validation.js
+++ Books.WebApp/Scripts/App/Resources/Validation.js
@@ -3,7 +3,8 @@
 extend(booksApp, {
 	resources: {
 		validation: {
-			imageFormatError: "Unsupported image format, only jpeg images are supported",
+			imageFormatError: "Only jpeg images are supported",
+			imageSizeError: "The image shouldn't be more than 200KB",
 			isbnError: "Incorrect ISBN number, example: 978-0804139021"
 		}
 	}
--- Books.WebApp/Scripts/App/ViewModels/BookViewModel.js
+++ Books.WebApp/Scripts/App/ViewModels/BookViewModel.js
@@ -1,114 +1,124 @@
 ﻿"use strict";
 
-var BookViewModel = function(book) {
-    var self = this;
+var BookViewModel = function (book) {
+	var self = this;
 
-    self.id = ko.observable(book.id);
+	self.id = ko.observable(book.id);
 
-    self.title = ko.observable(book.title);
-    self.title.extend({
-        required: true,
-        maxLength: 30
-    });
+	self.title = ko.observable(book.title);
+	self.title.extend({
+		required: true,
+		maxLength: 30
+	});
 
-    self.pageCount = ko.observable(book.pageCount);
-    self.pageCount.extend({
-        required: true,
-        min: 1,
-        max: 10000
-    });
+	self.pageCount = ko.observable(book.pageCount);
+	self.pageCount.extend({
+		required: true,
+		min: 1,
+		max: 10000
+	});
 
-    self.publisher = ko.observable(book.publisher);
-    self.publisher.extend({
-        maxLength: 30
-    });
+	self.publisher = ko.observable(book.publisher);
+	self.publisher.extend({
+		maxLength: 30
+	});
 
-    self.year = ko.observable(book.year);
-    self.year.extend({
-        required: true,
-        min: 1800,
-        max: 9999
-    });
+	self.year = ko.observable(book.year);
+	self.year.extend({
+		required: true,
+		min: 1800,
+		max: 9999
+	});
 
-    self.isbn = ko.observable(book.isbn);
-    self.isbn.extend({
-        checkIsbn: true
-    });
+	self.isbn = ko.observable(book.isbn);
+	self.isbn.extend({
+		checkIsbn: true
+	});
 
-    self.createDate = ko.observable(book.createDate);
+	self.createDate = ko.observable(book.createDate);
 
-    self.editDate = ko.observable(book.editDate);
+	self.editDate = ko.observable(book.editDate);
+
+	self.coverImageUploaderValue = ko.observable(null);
 
 	self.image = ko.observable(book.image);
 
-	self.hasImage = ko.pureComputed(function() {
+	self.hasImage = ko.pureComputed(function () {
 		return self.image() != null;
 	});
 
 	self.authors = ko.observableArray();
 
-    updateAuthors(book.authors);
+	updateAuthors(book.authors);
 
-    self.update = function (updBook) {
-        if (updBook == null) return;
+	self.update = function (updBook) {
+		if (updBook == null) return;
 
-        self.id(updBook.id);
-        self.title(updBook.title);
-        self.pageCount(updBook.pageCount);
-        self.publisher(updBook.publisher);
-        self.year(updBook.year);
-        self.isbn(updBook.isbn);
-        self.image(updBook.image);
-        self.createDate(updBook.createDate);
-        self.editDate(updBook.editDate);
+		self.id(updBook.id);
+		self.title(updBook.title);
+		self.pageCount(updBook.pageCount);
+		self.publisher(updBook.publisher);
+		self.year(updBook.year);
+		self.isbn(updBook.isbn);
+		self.image(updBook.image);
+		self.createDate(updBook.createDate);
+		self.editDate(updBook.editDate);
 
-        updateAuthors(updBook.authors);
-    }
+		updateAuthors(updBook.authors);
+	}
 
-    function updateAuthors(authors) {
-        self.authors.removeAll();
-        if (authors != null) {
-            ko.utils.arrayForEach(authors, function (author) {
-                self.authors.push(new AuthorViewModel(author));
-            });
-        }
-    }
+	function updateAuthors(authors) {
+		self.authors.removeAll();
+		if (authors != null) {
+			ko.utils.arrayForEach(authors, function (author) {
+				self.authors.push(new AuthorViewModel(author));
+			});
+		}
+	}
 
-    self.addAuthor = function () {
-        self.authors.push(new AuthorViewModel({
-	        bookId: self.id()
-        }));
-    }
+	self.addAuthor = function () {
+		self.authors.push(new AuthorViewModel({
+			bookId: self.id()
+		}));
+	}
 
-    self.deleteAuthor = function (author) {
-        self.authors.remove(author);
-    }
+	self.deleteAuthor = function (author) {
+		self.authors.remove(author);
+	}
 
-    self.uploadImage = function (myBook, e) {
-        var file = e.target.files[0];
-        var reader = new FileReader();
+	self.uploadImage = function (myBook, e) {
+		var file = e.target.files[0];
 
-        reader.addEventListener("load", function () {
-            self.image(reader.result);
-        });
+		if (!/^image\/jpeg$/.test(file.type)) {
+			alert(booksApp.resources.validation.imageFormatError);
+			return;
+		}
 
-        if (file && /^image\/jpeg$/.test(file.type)) {
-            reader.readAsDataURL(file);
-        } else {
-			alert(resources.validation.imageFormatError);
-        }
-    }
+		if ((file.size / 1024) > 200) {
+			alert(booksApp.resources.validation.imageSizeError);
+			return;
+		}
 
-    self.deleteImage = function () {
-        self.image(null);
-    }
+		var reader = new FileReader();
 
-    self.isValid = function () {
-        var errors = ko.validation.group(self, { deep: true });
+		reader.addEventListener("load", function () {
+			self.image(reader.result);
+		});
 
-        if (errors().length === 0) return true;
+		reader.readAsDataURL(file);
+	}
 
-        errors.showAllMessages();
-        return false;
-    }
+	self.deleteImage = function () {
+		self.image(null);
+		self.coverImageUploaderValue(null);
+	}
+
+	self.isValid = function () {
+		var errors = ko.validation.group(self, { deep: true });
+
+		if (errors().length === 0) return true;
+
+		errors.showAllMessages();
+		return false;
+	}
 }
